e68d5cb2a4269f79531a477ccf6d322159eb3cb9
James Cherti <60946298+jamescherti@users.noreply.github.com>
2023-03-22 13:48:02 -0400

    Switch to Apache Web server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+! [ -f /etc/apache2/envvars.default ] && cp /etc/apache2/envvars /etc/apache2/envvars.default
+cp /etc/apache2/envvars.default /etc/apache2/envvars
+# shellcheck disable=SC2154
+echo "export WikiDataDir=$WikiDataDir" >>/etc/apache2/envvars
 exec apache2ctl -D FOREGROUND
